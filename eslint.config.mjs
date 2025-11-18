9106cb8cb8b3c1936dbb3e088e332415058c885a
Heecheon Park <heecheon@thesesamelab.com>
2025-11-18 09:42:05 +0900

    testing the existing parallel route on next16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import unusedImports from "eslint-plugin-unused-imports";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -6,79 +7,73 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    parserOptions: {
-      project: ["tsconfig.json"],
-    },
-    plugins: {
-      "unused-imports": unusedImports,
-    },
-    rules: {
-      /* 0: off, 1: warn, 2: error */
-      "no-unused-vars": "off",
-      "prefer-const": "error",
-      "no-useless-catch": "off",
-      /* 
-        https://ota-meshi.github.io/eslint-plugin-regexp/rules/no-useless-escape.html
-        https://www.npmjs.com/package/eslint-plugin-regexp?activeTab=readme
-      */
-      "no-useless-escape": "warn",
-      "react-hooks/exhaustive-deps": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-key": [2, { checkFragmentShorthand: true }],
-      "react/jsx-props-no-spreading": "off",
-      // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-unstable-nested-components.md
-      "react/no-unstable-nested-components": [
-        "error", // <-- 성능 이슈를 일으키는 항목 발견시 에러로 처리
-        {
-          allowAsProps: true,
-        },
-      ],
-      "react/prop-types": [
-        0,
-        {
-          ignore: "className",
-        },
-      ],
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-inferrable-types": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-empty-function": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-      "unused-imports/no-unused-imports": "error",
-      "unused-imports/no-unused-vars": [
-        "warn",
-        {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_",
-        },
-      ],
-      "import/no-default-export": "error",
-    },
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  parserOptions: {
+    project: ["tsconfig.json"],
   },
-  {
-    files: [
-      "eslint.config.mjs",
-
-      /* Exclude NextJS special files */
-      "**/page.{jsx,tsx}",
-      "**/layout.{jsx,tsx}",
-      "**/loading.{jsx,tsx}",
-      "**/template.{jsx,tsx}",
-      "**/default.{jsx,tsx}",
+  plugins: {
+    "unused-imports": unusedImports,
+  },
+  rules: {
+    /* 0: off, 1: warn, 2: error */
+    "no-unused-vars": "off",
+    "prefer-const": "error",
+    "no-useless-catch": "off",
+    /* 
+      https://ota-meshi.github.io/eslint-plugin-regexp/rules/no-useless-escape.html
+      https://www.npmjs.com/package/eslint-plugin-regexp?activeTab=readme
+    */
+    "no-useless-escape": "warn",
+    "react-hooks/exhaustive-deps": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-key": [2, { checkFragmentShorthand: true }],
+    "react/jsx-props-no-spreading": "off",
+    // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-unstable-nested-components.md
+    "react/no-unstable-nested-components": [
+      "error", // <-- 성능 이슈를 일으키는 항목 발견시 에러로 처리
+      {
+        allowAsProps: true,
+      },
     ],
-    rules: {
-      "import/no-default-export": "off",
-    },
+    "react/prop-types": [
+      0,
+      {
+        ignore: "className",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "import/no-default-export": "error",
   },
-];
+}, {
+  files: [
+    "eslint.config.mjs",
+
+    /* Exclude NextJS special files */
+    "**/page.{jsx,tsx}",
+    "**/layout.{jsx,tsx}",
+    "**/loading.{jsx,tsx}",
+    "**/template.{jsx,tsx}",
+    "**/default.{jsx,tsx}",
+  ],
+  rules: {
+    "import/no-default-export": "off",
+  },
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
